b7e12fac24154f989ca2350bf942551323cc75e1
alfonsofdelarosa <alfonsofdelarosa@gmail.com>
2022-04-01 20:31:19 +0200

    changes productCard

--- src/components/productCard/productCard.jsx
+++ src/components/productCard/productCard.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import styles from './productCard.module.css';
 
 const ProductCard = (props) => {
-   return (<div className={styles.container}>
+   return (
+   <div className={styles.container}>
        <img src={props.picture} width= "120px" height={"120px"}/>
        <div>
            <b>{props.title}</b>
            <p>{props.description}</p>
-           <p>{props.priceMin}      {props.priceMax}</p>
+           <div className={styles.price}>
+            <p>{props.priceMin} €</p>
+            <p className={styles.priceMax}>{props.priceMax} €</p>
+            </div>
+           
         </div>
-   </div>)
+   </div>
+   )
 }
 export default ProductCard;
