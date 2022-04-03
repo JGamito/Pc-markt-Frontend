1d9ee1d0a4d5e5243dd540e18ac4c148d8506b20
JGamito <gamito90@hotmail.com>
2022-04-03 19:17:47 +0200

    new NavBar to connect client

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -4,41 +4,46 @@ import styles from "./navBar.module.css";
 import logo from "../../logo.png";
 
 const NavBar = () => {
-  return localStorage.getItem("token") ? (
+  return (
     <div className={styles.container}>
       <div>
         <Link to="/">
           <img className={styles.logo} alt="logo" src={logo} />
         </Link>
       </div>
-      <div className={styles.linksContainer}>
-        <Link className={styles.links} to="/">
-          Home
-        </Link>
-        <Link className={styles.links} to="/checkout">
-          Carrito
-        </Link>
-      </div>
-    </div>
-  ) : (
-    <div className={styles.container}>
-      <div>
-        <Link to="/">
-          <img className={styles.logo} alt="logo" src={logo} />
-        </Link>
-      </div>
-      <div className={styles.linksContainer}>
-        <Link className={styles.links} to="/">
-          Home
-        </Link>
-        <Link className={styles.links} to="/register">
-          Nuevo Usuario
-        </Link>
-        <Link className={styles.links} to="/login">
-          Identificarme
-        </Link>
-      </div>
+
+      {localStorage.getItem("token") ? (
+        <div className={styles.linksContainer}>
+          <Link className={styles.links} to="/">
+            Home
+          </Link>
+
+          <Link className={styles.links} to="/checkout">
+            Carrito
+          </Link>
+          <Link className={styles.links} to="/">
+            Desconectarme!
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.linksContainer}>
+          <Link className={styles.links} to="/">
+            Home
+          </Link>
+
+          <Link className={styles.links} to="/register">
+            Nuevo Usuario
+          </Link>
+          <Link className={styles.links} to="/login">
+            Identificarme
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
 export default NavBar;
+
+// delete TOKEN en Link "Desconectarme!"           Storage.removeItem("token")
+// Actualizar la NavBar tras el Login para que detecte el token!!!
+//
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -14,9 +14,11 @@ const Register = () => {
   const [postalCode, setPostalCode] = useState("");
   const [streetExtra, setStreetExtra] = useState("");
   const [phone, setPhone] = useState("");
+  const [status, setStatus] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setStatus = "active";
     const user = {
       name,
       surname,
@@ -28,41 +30,49 @@ const Register = () => {
       postalCode,
       streetExtra,
       phone,
+      status,
     };
 
-    fetch("https://pcmarkt.herokuapp.com/login/login", {
+    fetch("https://pcmarkt.herokuapp.com/users/", {
       method: "POST",
       headers: { "content-type": "application/json" },
       body: JSON.stringify(user),
     });
+    //    navigate("/login/login");
   };
 
   return (
     <div className={styles.father}>
-      <form className={styles.body} onSubmit={handleSubmit}>
-        <div className={styles.formulary}>
-          <h4>Crear cuenta</h4>
-        </div>
-        <div className={styles.input}>
+      <div>
+        <h4>Crear cuenta</h4>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.inputs}>
+          <label>Nombre:</label>
           <input
+            className={styles.input}
             value={name}
             onChange={(event) => setName(event.target.value)}
             type="text"
             placeholder="Nombre"
           />
+
           <input
+            className={styles.input}
             value={surname}
             onChange={(event) => setSurname(event.target.value)}
             type="text"
             placeholder="Apellidos"
           />
           <input
+            className={styles.input}
             value={email}
             onChange={(event) => setEmail(event.target.value)}
             type="text"
             placeholder="E-mail"
           />
           <input
+            className={styles.input}
             value={password}
             onChange={(event) => setPassword(event.target.value)}
             type="password"
@@ -70,36 +80,43 @@ const Register = () => {
           />
           <h5> Dirección:</h5>
           <input
+            className={styles.input}
             value={street}
             onChange={(event) => setStreet(event.target.value)}
             type="text"
             placeholder="Calle / numero / piso"
           />
           <input
+            className={styles.input}
             value={city}
             onChange={(event) => setCity(event.target.value)}
             type="text"
             placeholder="Localidad"
           />
           <input
+            className={styles.input}
             value={province}
             onChange={(event) => setProvince(event.target.value)}
             type="text"
             placeholder="Provincia"
           />
           <input
+            className={styles.input}
             value={postalCode}
             onChange={(event) => setPostalCode(event.target.value)}
             type="text"
             placeholder="Codigo Postal"
+            pattern="[0-9]{5}"
           />
           <input
+            className={styles.input}
             value={streetExtra}
             onChange={(event) => setStreetExtra(event.target.value)}
             type="text"
             placeholder="Direccion alternativa"
           />
           <input
+            className={styles.input}
             value={phone}
             onChange={(event) => setPhone(event.target.value)}
             type="tel"
@@ -108,9 +125,11 @@ const Register = () => {
           />
 
           <br />
-          <button className={styles.button} type="submit">
-            registrarse!
-          </button>
+          <div className={styles.buttonDiv}>
+            <button className={styles.btn} type="submit">
+              Crear cuenta
+            </button>
+          </div>
           <br />
         </div>
       </form>
